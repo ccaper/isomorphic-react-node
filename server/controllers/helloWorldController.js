ca2e106333848f17482f74b01bd962ba8950fcd9
ccaper <christian.caperton@gmail.com>
2018-02-17 10:37:22 -0600

    added rest endpoint for a post with a path var and body

--- server/controllers/helloWorldController.js
+++ server/controllers/helloWorldController.js
@@ -1,4 +1,19 @@
+const moment = require('moment');
 
 exports.helloWorld = (req, res) => {
   res.json({ helloWorld: true });
+};
+
+exports.pathBodyTest = (req, res) => {
+  console.log(req.body);
+  const { id } = req.params;
+  const { value1, value2 } = req.body;
+  const now = moment();
+  const result = {
+    id,
+    value1,
+    value2,
+    date: now.format()
+  };
+  res.json(result);
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,5 +5,6 @@ const helloWorldController = require('../controllers/helloWorldController');
 const router = express.Router();
 
 router.get('/api/helloworld', helloWorldController.helloWorld);
+router.post('/api/pathBodyTest/:id', helloWorldController.pathBodyTest);
 
 module.exports = router;
