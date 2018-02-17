09d530a02814916c4825fb543e28e7d02f8545e3
ccaper <christian.caperton@gmail.com>
2018-02-17 09:50:52 -0600

    brought in error page using pug mainly for 404 errors, and uses envrionment var set in variables file to control printing stack trace

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const passport = require('passport');
@@ -9,6 +10,10 @@ const errorHandlers = require('./server/handlers/errorHandlers');
 
 // create our Express app
 const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, '/server/views'));
+app.set('view engine', 'pug');
 
 // Takes the raw requests and turns them into usable properties on req.body
 app.use(bodyParser.json());
@@ -32,6 +37,15 @@ app.use('/', routes);
 
 // If that above routes didnt work, we 404 them and forward to error handler
 app.use(errorHandlers.notFound);
+
+// Otherwise this was a really bad error we didn't expect! Shoot eh
+if (app.get('env') === 'development') {
+  /* Development Error Handler - Prints stack trace */
+  app.use(errorHandlers.developmentErrors);
+}
+
+// production error handler
+app.use(errorHandlers.productionErrors);
 
 // done! we export it so we can start the site in start.js
 module.exports = app;
--- server/handlers/errorHandlers.js
+++ server/handlers/errorHandlers.js
@@ -8,3 +8,39 @@ exports.notFound = (req, res, next) => {
   err.status = 404;
   next(err);
 };
+
+/*
+  Development Error Handler
+
+  In development we show good error messages so if we hit a syntax error or any other previously un-handled error, we can show good info on what happened
+*/
+exports.developmentErrors = (err, req, res, next) => {
+  err.stack = err.stack || '';
+  const errorDetails = {
+    message: err.message,
+    status: err.status,
+    stackHighlighted: err.stack.replace(/[a-z_-\d]+.js:\d+:\d+/gi, '<mark>$&</mark>')
+  };
+  res.status(err.status || 500);
+  res.format({
+    // Based on the `Accept` http header
+    'text/html': () => {
+      res.render('error', errorDetails);
+    }, // Form Submit, Reload the page
+    'application/json': () => res.json(errorDetails) // Ajax call, send JSON back
+  });
+};
+
+/*
+  Production Error Handler
+
+  No stacktraces are leaked to user
+*/
+exports.productionErrors = (err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    status: err.status || 500,
+    message: err.message,
+    error: {}
+  });
+};
