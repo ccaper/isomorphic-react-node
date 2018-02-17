6ae9ac19b5a3deb354f07f36177af0f1c5478b9d
ccaper <christian.caperton@gmail.com>
2018-02-17 14:48:14 -0600

    simple react component now up!

---
+++ client/isomorphic-react-node.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
+
+import HelloWorld from './components/HelloWorld';
+import NotFound from './components/NotFound';
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={HelloWorld} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  );
+};
+
+render(<Root />, document.querySelector('#main'));
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: './client/isomorphic-react-node.js',
+  output: {
+    path: __dirname,
+    filename: './public/static/bundle.js'
+  },
+  context: __dirname,
+  resolve: {
+    extensions: ['.js', '.json', '*']
+  },
+  module: {
+    rules: [{
+      test: /js?$/,
+      exclude: /(node_modules|bower_components)/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', 'es2015', 'stage-2']
+        }
+      }]
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }
+    ]
+  }
+};
