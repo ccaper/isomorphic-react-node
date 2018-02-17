c75b82d631fe6d2418cdf638bdbcaa93f7a67f02
ccaper <christian.caperton@gmail.com>
2018-02-17 16:36:01 -0600

    fetch now working and page updating

--- client/components/HelloWorld.js
+++ client/components/HelloWorld.js
@@ -1,9 +1,38 @@
 import React from 'react';
 
+function fetchData() {
+  return fetch('api/pathBodyTest/2', {
+    method: 'post',
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({value1: 1, value2: 2})
+  });
+}
+
 class HelloWorld extends React.Component {
+  state = {
+    helloWorld: {}
+  };
+
+  componentDidMount() {
+    fetchData()
+      .then(res => res.json())
+      .then(data => this.setState({helloWorld: data}));
+  }
+
   render() {
     return (
-      <h2>Hello World!</h2>
+      <div>
+        <h2>Hello World!</h2>
+          <p>data</p>
+          <ul>
+            {Object.keys(this.state.helloWorld).map(
+              key => (<li key={key}>{key}: {this.state.helloWorld[key]}</li>)
+            )}
+          </ul>
+      </div>
     );
   }
 }
